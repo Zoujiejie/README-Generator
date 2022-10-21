8579153a96602353160329299c2525bec11ce761
katniss邹洁 <katnisszoujie@MacBook-Pro.local>
2022-10-21 15:30:05 -0700

    prompt questions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const utils = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        message: "Please enter description of your project:",
+        name: "description",
+    },
+//Table of Contents TBD
+    {
+        type: "input",
+        message: "What are the installation instructions for your project?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "What are the instructions to use your project??",
+        name: "usage",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select the license you used for this project.",
+        choices: [
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "Mozilla",
+            "MIT",
+            "Apache",
+            "Boost",
+        ],
+    },
+    {
+        type: "input",
+        message: "Please explain how other developers or resources might contribute to your project?",
+        name: "contribution",
+    },
+    {
+        type: "input",
+        name: "author",
+        message: "What is your name?",
+    },
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "userEmail",
+        message: "What is your email?",
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Please list how to contact you for thos who have questions",
+    },
+//tests TBD
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
